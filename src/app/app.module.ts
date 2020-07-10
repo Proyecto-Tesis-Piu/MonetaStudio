2181e929796008df551f63b36719370f68e84f59
Erick <erick_rr_09@hotmail.com>
2020-07-09 21:53:45 -0600

    Crear usuario e Iniciar Sesion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './Components/Shared/navbar/navbar.component';
 import { HomeComponent } from './Components/home/home.component';
 import { AppComponent } from './app.component';
-import { CrearUsuarioComponent } from './Components/crear-usuario/crear-usuario.component';
-import { IniciarSesionComponent } from './Components/iniciar-sesion/iniciar-sesion.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CrearUsuarioComponent } from './Components/Usuarios/crear-usuario/crear-usuario.component';
+import { IniciarSesionComponent } from './Components/Usuarios/iniciar-sesion/iniciar-sesion.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    CrearUsuarioComponent,
     IniciarSesionComponent,
+    CrearUsuarioComponent,
   ],
   imports: [
     BrowserModule,
