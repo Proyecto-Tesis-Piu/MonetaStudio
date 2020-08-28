55d8a458c61f84ad41d5be1fd1779908c15d19c2
Piu <erickpacheco114@hotmail.com>
2020-08-27 19:55:05 -0500

    transasctions ingresos egresos

---
+++ src/app/Components/Transactions/transaction.model.ts
@@ -0,0 +1,9 @@
+export class Transaction {
+    concept: string = "";
+    date: Date = new Date();
+    amount: number = 0;
+    categoryIcon: string = "";
+    categoryName: string = "";
+    userId: string = "";
+    isExpense: boolean = true;
+}
--- src/app/Components/Transactions/transactions.component.ts
+++ src/app/Components/Transactions/transactions.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { temp } from './temp.model'
-import { MatProgressSpinner } from '@angular/material/progress-spinner'
+import { Transaction } from "./transaction.model";
 
 @Component({
   selector: 'app-transactions',
@@ -9,37 +9,99 @@ import { MatProgressSpinner } from '@angular/material/progress-spinner'
 })
 export class TransactionsComponent implements OnInit {
 
-  transactions:temp[] = [];
+  transactions: temp[] = [];
   constructor() {
-    var aux:temp = new temp();
+    var aux: temp = new temp();
     aux.color = "#fff";
     aux.value = 100;
     this.transactions.push(aux);
-    var aux:temp = new temp();
+    var aux: temp = new temp();
     aux.color = "accent";
     aux.value = 80;
     this.transactions.push(aux);
-    var aux:temp = new temp();
+    var aux: temp = new temp();
     aux.color = "warn";
     aux.value = 60;
     this.transactions.push(aux);
-    var aux:temp = new temp();
+    var aux: temp = new temp();
     aux.color = "#999999";
     aux.value = 40;
     this.transactions.push(aux);
-    var aux:temp = new temp();
+    var aux: temp = new temp();
     aux.color = "#777777";
     aux.value = 20;
     this.transactions.push(aux);
-   }
-   public selectedVal: string;
+  }
+  public selectedVal: string;
 
-   ngOnInit(){
-    this.selectedVal ='option1';
-  } 
-  
+  ngOnInit() {
+    this.selectedVal = 'option1';
+  }
+
   public onValChange(val: string) {
     this.selectedVal = val;
   }
 
+  transactionsARR: Transaction[] = [
+    {
+      concept: 'Luz',
+      date: new Date('8/03/2020'),
+      amount: 400,
+      categoryIcon: "home",
+      categoryName: "vivienda",
+      userId: "",
+      isExpense: true
+    },
+
+    {
+      concept: 'uber',
+      date: new Date('8/03/2020'),
+      amount: 148.32,
+      categoryIcon: "directions_car",
+      categoryName: "Transporte",
+      userId: "",
+      isExpense: true
+    },
+
+    {
+      concept: 'Cine',
+      date: new Date('8/03/2020'),
+      amount: 86,
+      categoryIcon: "live_tv",
+      categoryName: "Entretenimiento",
+      userId: "",
+      isExpense: true
+    },
+    {
+      concept: 'Despensa',
+      date: new Date('8/03/2020'),
+      amount: 756,
+      categoryIcon: "local_grocery_store",
+      categoryName: "Despensa",
+      userId: "",
+      isExpense: true
+    },
+
+    {
+      concept: 'Salud',
+      date: new Date('8/03/2020'),
+      amount: 1547,
+      categoryIcon: "local_hospital",
+      categoryName: "Salud",
+      userId: "",
+      isExpense: true
+    },
+
+    {
+      concept: 'Tenis lacoste',
+      date: new Date('8/03/2020'),
+      amount: 1500,
+      categoryIcon: "watch",
+      categoryName: "Ropa y Accesorios",
+      userId: "",
+      isExpense: true
+    },
+  ]
 }
+
+
