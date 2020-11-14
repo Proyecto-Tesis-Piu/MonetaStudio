c4225e7dc9fb76b4c67d29fc3cfbd138ff241ce3
Piu <erickpacheco114@hotmail.com>
2020-11-14 15:32:20 -0700

    Merge Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,35 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent{
   title = 'Moneta Studio';
-<<<<<<< HEAD
-  openSidenav = false;
-  token: String;
-  tokenSubscription: Subscription;
-
-
-  constructor(public dialog: MatDialog,
-    protected storageMap: StorageMap) {
-    this.tokenSubscription = this.storageMap.watch('token', { type: 'string' }).subscribe((data: String) => {
-      this.token = data;
-    });
-  }
-
-  logout(): void {
-    this.storageMap.delete('token').subscribe(() => {});
-    localStorage.removeItem('token');
-    window.location.reload();
-  }
-
-  openDialog(): void {
-    const dialogRef = this.dialog.open(LoginDialogComponent, {});
-  }
-
-  openDialog2(): void {
-    const dialogRef = this.dialog.open(RegistrationComponent, {});
-  }
-=======
 
   constructor() { }
 
->>>>>>> abf854106cd465fc729f56133cb86e21bb7eefa6
 }
