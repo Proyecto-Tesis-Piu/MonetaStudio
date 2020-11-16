14ecb23e2c502d6b10bdca9235b56ede8895cf13
Raul Rodrigo Perez Marrero <raul96p@outlook.com>
2020-11-16 14:18:53 -0600

    .

--- src/app/Components/Transactions/transactions.component.ts
+++ src/app/Components/Transactions/transactions.component.ts
@@ -135,13 +135,13 @@ export class TransactionsComponent implements OnInit, OnDestroy {
   public incomeChartColors: Array<any>;
 
   public chartType: string = 'doughnut';
-  public chartColors: Array<any> = [
+  /* public chartColors: Array<any> = [
     {
       backgroundColor: ['#F7464A', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'],
       hoverBackgroundColor: ['#FF5A5E', '#5AD3D1', '#FFC870', '#A8B3C5', '#616774'],
       borderWidth: 2,
     }
-  ];
+  ]; */
   public chartOptions: any = {
     responsive: true,
     tooltips: {
