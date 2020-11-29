1c1ebbc2827cd427099882b77c3b2478f44ebb94
Raul Rodrigo Perez Marrero <raul96p@outlook.com>
2020-11-29 13:20:53 -0600

    confirm mail functional changes.

--- src/app/Components/Users/shared/user.service.ts
+++ src/app/Components/Users/shared/user.service.ts
@@ -53,7 +53,7 @@ export class UserService extends BaseService {
 
   public confirmEmail(token: String){
     var tokenHeader = new HttpHeaders({'Authorization':'Bearer ' + token});
-    return this.http.get(this.serviceUrl + '/ConfirmEmail', {headers : tokenHeader});
+    return this.http.get(this.serviceUrl + '/ConfirmEmail', { headers: tokenHeader });
   }
 
   public submitFeedback(feedback: Feedback, token: string){
